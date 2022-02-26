f9c60351f09be8b0478b035009ff0b27e70223a3
Steve Datz <sfdatz@live.com>
2022-02-26 11:00:11 -0600

    local euler fix and prize sub rotation

--- Assets/Scripts/BonusWheel.cs
+++ Assets/Scripts/BonusWheel.cs
@@ -9,11 +9,13 @@ public class BonusWheel : MonoBehaviour
     [Header("Components")]
     [SerializeField] private Button _spinButton;
     [SerializeField] private Transform _wheelTransform;
+    [SerializeField] private GameObject[] _prizes;
     [Header("Settings")] [Range(0.5f, 5f)]
     [SerializeField]private float _spinTime = 2.5f;
+    [SerializeField]private int _numRotations = 4;
     [SerializeField] private AnimationCurve _spinCurve;
     
-    private const float SINGLE_ROTATION_ANGLE = 22.5f;
+    private float _singleRotationAngle = 22.5f;
     private const int NUM_PRIZES = 8;
     
     private bool _isSpinning;
@@ -22,6 +24,8 @@ public class BonusWheel : MonoBehaviour
     
     public void Start()
     {
+        _singleRotationAngle = 360f / NUM_PRIZES;
+        SetPrizeDisplay();
         _spinButton.onClick.AddListener(SpinWheel);
     }
 
@@ -37,15 +41,15 @@ public class BonusWheel : MonoBehaviour
     private int GetIndexOfRandomPrize()
     {
         //TODO- base off dropchances
-        return UnityEngine.Random.Range(0, 8);
+        return UnityEngine.Random.Range(1, 8);
     }
     
     IEnumerator WheelRotation(int indexOfPrize)
     {
         _isSpinning = true;
         _spinButton.interactable = false;
-        float endingAngle = (NUM_PRIZES * 360) + SINGLE_ROTATION_ANGLE + indexOfPrize;
-        //float startingAngle = _wheelTransform.eulerAngles.z;
+        float startingAngle = _wheelTransform.eulerAngles.z;
+        float endingAngle = (_numRotations * 360f) + _singleRotationAngle * indexOfPrize - startingAngle;
         Debug.Log($"Chose Index={indexOfPrize} and endingAngle={endingAngle}");
         float currTime = 0;
 
@@ -54,14 +58,22 @@ public class BonusWheel : MonoBehaviour
             currTime += Time.deltaTime;
             yield return new WaitForEndOfFrame();
             float spinningAngle = endingAngle * _spinCurve.Evaluate(currTime / _spinTime);
-            _wheelTransform.eulerAngles = new Vector3(0, 0 ,spinningAngle);
+            _wheelTransform.localEulerAngles = new Vector3(0, 0 ,spinningAngle + startingAngle);
         }
 
-        _wheelTransform.eulerAngles = new Vector3(0, 0 ,endingAngle);
+        //_wheelTransform.eulerAngles = new Vector3(0, 0 ,endingAngle + startingAngle);
 
 
         _spinButton.interactable = true;
         _isSpinning = false;
+    }
+
+    private void SetPrizeDisplay()
+    {
+        for (int i = 0; i < _prizes.Length; i++)
+        {
+            _prizes[i].transform.eulerAngles = new Vector3(0, 0, -360f / NUM_PRIZES * i);
+        }
     }
     
     
